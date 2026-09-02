e357071acfb2a003204661a8403e42b566508f68
nurican26 <btaanaliz@gmail.com>
2026-09-02 14:53:40 +0300

    Change Excel file extension from .xlsx to .xls

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.title("⚡ Sinyal Takip Merkezi")
 st.markdown("---")
 st.subheader("Sinyal Üretim Merkezi")
 
-EXCEL_FILE_PATH = "nurcan.xlsx" 
+EXCEL_FILE_PATH = "nurcan.xls" 
 
 col1, col2 = st.columns(2)
 with col1:
