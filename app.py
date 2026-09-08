b216bf84df6f0745aaa807d6036c48b9cae197c4
nurican26 <btaanaliz@gmail.com>
2026-09-08 14:03:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,14 @@
                 if aranan_hisse != "Seçiniz...":
                     h_detay_veri = yf.Ticker(f"{aranan_hisse}.IS").history(period="1d", timeout=2)
                     if len(h_detay_veri) > 0:
-                        # Gerekli fiyat verilerini çekiyoruz
                         guncel_fiyat = float(h_detay_veri['Close'].iloc[-1])
                         en_yuksek = float(h_detay_veri['High'].iloc[-1])
                         en_dusuk = float(h_detay_veri['Low'].iloc[-1])
                         
-                        # Yan yana 3 sütun halinde gösteriyoruz
                         m_col1, m_col2, m_col3 = st.columns(3)
                         m_col1.metric("Güncel Fiyat", f"{guncel_fiyat:,.2f} TL")
                         m_col2.metric("En Yüksek Fiyat", f"{en_yuksek:,.2f} TL")
                         m_col3.metric("En Düşük Fiyat", f"{en_dusuk:,.2f} TL")
+                        
+        # --- TOPLAM GİRİŞ SAYACI ---
+        st.markdown(f'<p class="kucuk-sayac">📊 Oda Toplam Giriş Yapanlar: {st.session_state["topham_sayac"]}</p>', unsafe_allow_html=True)
