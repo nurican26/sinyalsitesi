808f4c589907f0f5bbc22c62f8ee58848fedf637
nurican26 <btaanaliz@gmail.com>
2026-09-09 11:58:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ if os.path.exists(excel_yolu):
             except: continue
             
         tablo_html += '</table>'
-        st.markdown('<p style="font-size:18px; font-weight:bold; color:#1E90FF;">📈 BTA ALGORİTMİK HİSSE </p>', unsafe_allow_html=True)
+       st.markdown('<p style="font-size:18px; font-weight:bold; color:#FFA500;">🔍 BIST HİSSE ARAMA MOTORU</p>', unsafe_allow_html=True)
+📈 BTA ALGORİTMİK HİSSE </p>', unsafe_allow_html=True)
         if veri_var_mi: st.markdown(tablo_html, unsafe_allow_html=True)
         
         # --- BORSA ARAMA MOTORU ---
