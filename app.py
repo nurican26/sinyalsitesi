4f4c4b466ad7b8811ede5db2fde3c332421ffe5a
nurican26 <btaanaliz@gmail.com>
2026-09-14 22:03:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from streamlit_autorefresh import st_autorefresh
 # SAYFA AYARLARI
 # ==================================================
 st.set_page_config(
-    page_title="BTA Algoritmik İşlem Analiz Portalı",
+    page_title="BTA Algoritmik İşlem Analizi",
     page_icon="📈",
     layout="wide",
     initial_sidebar_state="collapsed"
